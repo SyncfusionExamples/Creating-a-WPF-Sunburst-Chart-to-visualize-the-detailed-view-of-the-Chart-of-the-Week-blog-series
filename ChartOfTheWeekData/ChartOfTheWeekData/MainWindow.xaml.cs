c1cbb6398f6840292ad383c9390832a98c683263
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-05-08 19:15:30 +0530

    Added the sample

---
+++ ChartOfTheWeekData/ChartOfTheWeekData/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Syncfusion.UI.Xaml.SunburstChart;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace ChartOfTheWeekData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            var hyperlink = (Hyperlink)sender;
+            var url = hyperlink.NavigateUri.AbsoluteUri;
+            System.Diagnostics.Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private bool isSegmentSelected = false;
+
+        private void SfSunburstChart_SelectionChanged(object sender, SunburstSelectionChangedEventArgs e)
+        {
+            var selectedSegment = e.SelectedSegment;
+            string? segmentName = "";
+
+            if (selectedSegment != null && !isSegmentSelected)
+            {
+                isSegmentSelected = true;
+                segmentName = (selectedSegment.Parent?.Category ?? e.SelectedSegment.Category)?.ToString();
+                viewModel.SelectedBlogs.Clear();
+                var blogsToAdd = viewModel.Blogs.Where(b => b.Platform == segmentName).ToList();
+
+                foreach (var blog in blogsToAdd)
+                {
+                    viewModel.SelectedBlogs.Add(blog);
+                }
+
+                listView.Items.Refresh();
+            }
+            else if (isSegmentSelected)
+            {
+                viewModel.SelectedBlogs.Clear();
+                foreach (var blog in viewModel.Blogs)
+                {
+                    viewModel.SelectedBlogs.Add(blog);
+                }
+
+                listView.Items.Refresh();
+                isSegmentSelected = false;
+            }
+        }
+    }
+}
---
+++ ChartOfTheWeekData/ChartOfTheWeekData/Model/ArticleModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartOfTheWeekData
+{
+    public class ArticleModel
+    {
+        public string? Platform { get; set; }
+        public double Count { get; set; }
+        public string? Icon { get; set; }
+        public string? Category { get; set; }
+        public string? Name { get; set; }
+        public string? URL { get; set; }
+
+        // Chart ItemSource
+        public ArticleModel(string platform, double count , string category)
+        {
+            Platform = platform;
+            Count = count;
+            Category = category;
+        }
+
+        // ListView ItemSource
+        public ArticleModel(string platform,string icon, string category, string name, string url)
+        {
+            Platform = platform;
+            Icon = icon;
+            Category = category;
+            Name = name;
+            URL = url;
+        }
+    }
+}
