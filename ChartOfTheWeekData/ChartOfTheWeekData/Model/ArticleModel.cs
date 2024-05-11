0a58f6564332ad37f70e99095c1f474b6a8e9c91
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-05-12 00:35:08 +0530

    Updated the view model

--- ChartOfTheWeekData/ChartOfTheWeekData/Model/ArticleModel.cs
+++ ChartOfTheWeekData/ChartOfTheWeekData/Model/ArticleModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChartOfTheWeekData
+﻿namespace ChartOfTheWeekData
 {
     public class ArticleModel
     {
@@ -15,22 +9,18 @@ namespace ChartOfTheWeekData
         public string? Name { get; set; }
         public string? URL { get; set; }
 
+        // ListView ItemSource
+        public ArticleModel()
+        {
+
+        }
+
         // Chart ItemSource
         public ArticleModel(string platform, double count , string category)
         {
             Platform = platform;
             Count = count;
             Category = category;
-        }
-
-        // ListView ItemSource
-        public ArticleModel(string platform,string icon, string category, string name, string url)
-        {
-            Platform = platform;
-            Icon = icon;
-            Category = category;
-            Name = name;
-            URL = url;
         }
     }
 }
